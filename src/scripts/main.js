82cf2624b5e765c0f5cd66ce3fb2b44e7f1dd1e5
Luan <luam10@hotmail.com.br>
2024-10-11 17:58:46 -0300

    criando a contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
-console.log('Ola mundo')
+AOS.init();
+
+const dataDoEvento = new Date('Dec 12, 2024 19:00:00');
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const interval = setInterval(function(){
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+    const distanciaEvento = timeStampDoEvento - timeStampAtual;
+
+    const dia = Math.floor(distanciaEvento / (1000 * 60 * 60 * 24));
+    const horas = Math.floor((distanciaEvento % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutos = Math.floor((distanciaEvento % (1000 * 60 * 60)) / (1000 * 60));
+    const segundos = Math.floor((distanciaEvento % (1000 * 60)) / 1000);
+
+
+    document.getElementById('contador').innerHTML = `${dia}d ${horas}h ${minutos}m ${segundos}s`
+
+    if(distanciaEvento < 0){
+        clearInterval(interval)
+        document.getElementById('contator').innerHTML = `Evendo expirado`
+    }
+}, 1000)
+
